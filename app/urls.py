2866ac0e3151038f5cb7da26202d607423432a70
Alibek Alikhan <alialikhan0520@gmail.com>
2023-05-17 17:58:04 +0600

    #16 Привезал url с api

--- app/urls.py
+++ app/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     # path('__debug__/', include('debug_toolbar.urls')),
     # path('auth/', include('accounts.urls')),
     # path("", include('webapp.urls')),
-    # path('api/', include('api.urls')),
+    path('api/', include('api.urls')),
 ]
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
 #     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
