29b321516385943470df248a294991db6d9c03cb
Alibek Beket <alibek1beket@gmail.com>
2023-05-22 19:29:26 +0600

    #26 Поменял модель мероприятий
    Заменил два поля многие ко многим(бронированные пользователи и оплатившие пользователи) на одно поле многие ко многим(забронированные пользователи) с дополнительными поля(дата брони, дата оплаты, дата окончаня брони). Обновил serializers для мероприятий и создал serializer для модели бронирования. Так же все поля в новой модели добавил в админку и обновил фикстуры.

--- webapp/models/__init__.py
+++ webapp/models/__init__.py
@@ -1,6 +1,6 @@
 from .cities import Cities
 from .type_events import TypeEvents
-from .events import Events
+from .events import Events, UserBooked
 from .image import Image
 from .news import News
 
--- webapp/models/events.py
+++ webapp/models/events.py
@@ -1,61 +1,67 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Events(models.Model):
     name = models.CharField(
         max_length=200,
         null=False,
+        blank=False,
         verbose_name="Наименование"
     )
     cities = models.ForeignKey(
         to='webapp.Cities',
         related_name='сities',
         on_delete=models.CASCADE,
-        verbose_name="Город"
+        verbose_name="Город",
+        null=True,
+        blank=True
     )
     type_events = models.ForeignKey(
         to='webapp.TypeEvents',
         related_name='type_events',
         on_delete=models.CASCADE,
-        verbose_name="Тип мероприятия"
+        verbose_name="Тип мероприятия",
+        null=False,
+        blank=False
     )
     events_at = models.DateTimeField(
         verbose_name="Дата мероприятия",
         null=False,
-        default=None
+        blank=False
     )
-    user = models.ManyToManyField(
+    sponsor = models.ForeignKey(
         to=get_user_model(),
-        related_name='user',
+        related_name='sponsor',
         blank=True,
+        null=True,
+        on_delete=models.CASCADE,
         verbose_name='Организатор'
     )
     number_of_seats = models.IntegerField(
+        null=False,
+        blank=False,
+        validators=[MinValueValidator(2), MaxValueValidator(100)],
         verbose_name="Количество мест"
     )
     start_register_at = models.DateTimeField(
-        verbose_name="дата начала регистрации",
+        verbose_name="Дата начала регистрации",
         null=True,
         default=None
     )
     end_register_at = models.DateTimeField(
-        verbose_name="дата конца регистрации",
+        verbose_name="Дата конца регистрации",
         null=True,
-        default=None
+        blank=True
     )
-    user_booked = models.ManyToManyField(
+    resident_booked = models.ManyToManyField(
         to=get_user_model(),
+        through='UserBooked',
         related_name='user_booked',
         blank=True,
-        verbose_name='забронировали_место'
-    )
-    user_paid = models.ManyToManyField(
-        to=get_user_model(),
-        related_name='user_paid',
-        blank=True,
-        verbose_name='оплатили'
+        verbose_name='Бранированные резиденты'
     )
     description = models.TextField(
         max_length=3000,
@@ -74,7 +80,7 @@ class Events(models.Model):
         verbose_name="Цена"
     )
     is_deleted = models.BooleanField(
-        verbose_name="удалено",
+        verbose_name="Удалено",
         null=False,
         default=False
     )
@@ -100,4 +106,36 @@ class Events(models.Model):
         self.save()
 
     def __str__(self):
-        return f"{self.name} - {self.start_register_at}"
+        return f"Event {self.name} - started {self.events_at}"
+
+
+class UserBooked(models.Model):
+    resident = models.ForeignKey(
+        to=get_user_model(),
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        verbose_name='Забронировал'
+    )
+    event = models.ForeignKey(
+        to=Events,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        verbose_name='Мероприятие'
+    )
+    booking_date = models.DateTimeField(
+        null=False,
+        blank=False
+    )
+    date_of_payment = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+    cancellation_date = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f"resident id-{self.resident} booking in events id-{self.event}"
