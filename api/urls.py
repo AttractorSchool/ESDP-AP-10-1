9930c71e99e334fd1dec26ea91cc076433c0fab6
Valentin Kim <valentink@yandex.ru>
2023-06-05 20:11:54 +0600

    #40 в urls.py добавлен путь api
    для просмотра списка отзывов из модели Review

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from api.views.events_api_view import EventsSimpleView
 from api.views.news_api_view import NewsSimpleView
+from api.views.reviews_api_view import ReviewsSimpleView
 
 urlpatterns = [
-    path('events/', EventsSimpleView.as_view(), name="events_list"),
-    path('news/', NewsSimpleView.as_view(), name="news_list"),
+    path("events/", EventsSimpleView.as_view(), name="events_list"),
+    path("news/", NewsSimpleView.as_view(), name="news_list"),
+    path("reviews/", ReviewsSimpleView.as_view(), name="reviews_list"),
 ]
