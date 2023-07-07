5ddc1cc355977f7b4588a9e09350072da5e4fa43
Valentin Kim <valentink@yandex.ru>
2023-07-07 12:55:01 +0600

    #112 Созданы формы forms.py для CRUD операций для модели News
    и добавлены пути в urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from webapp.views.events import EventDetailView
+from webapp.views.news import NewsCreateView, NewsDetail, NewsUpdateView, NewsDeleteView
+from webapp.views.events import EventDetailView, EventsCreateView, EventsUpdateView
 from webapp.views.newsline import NewslineView
 from webapp.views.profile import ProfileListView, ProfileDetailView
 
 urlpatterns = [
     path('newsline/', NewslineView.as_view(), name="newsline"),
+    path('news/create/', NewsCreateView.as_view(), name='news_create'),
+    path('news/<int:pk>/', NewsDetail.as_view(), name='news_detail'),
+    path('news/update/<int:pk>/', NewsUpdateView.as_view(), name='news_update'),
+    path('news/delete/<int:pk>/', NewsDeleteView.as_view(), name='news_delete'),
+    path('news/confirm_delete/<int:pk>/', NewsDeleteView.as_view(), name='news_confirm_delete'),
     path('accounts/', ProfileListView.as_view(), name='account_list'),
     path('accounts/<int:pk>/', ProfileDetailView.as_view(), name='account_detail'),
-    path('events/<int:pk>/', EventDetailView.as_view(), name='events_detail')
+    path('events/<int:pk>/', EventDetailView.as_view(), name='events_detail'),
+    path('events/create/', EventsCreateView.as_view(), name='events_create'),
+    path('events/update/<int:pk>/', EventsUpdateView.as_view(), name='events_update'),
 ]
