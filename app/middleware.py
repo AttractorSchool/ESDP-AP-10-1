fefb001b8977c7eba4afaf9f366b0bcdfcf4e277
Daniyar Otegen <daniyarotegen@gmail.com>
2023-07-10 15:07:14 +0600

    #78 Настройка Centrifugo для авторизации через JWT токены и интергация чата
    - Исправлен баг с необходимостью перезагрузки страницы для логина

--- app/middleware.py
+++ app/middleware.py
@@ -6,51 +6,31 @@ from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from accounts.cookie_auth import CookieJWTAuthentication
 
 
-class CookieJWTAuthenticationMiddleware(MiddlewareMixin):
+class CombinedMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        print("CookieJWTAuthenticationMiddleware has been called")
         if request.path != reverse('login'):
+            raw_refresh_token = request.COOKIES.get('refresh_jwt')
+
+            if raw_refresh_token is not None:
+                try:
+                    refresh = RefreshToken(raw_refresh_token)
+                    new_tokens = refresh.access_token
+
+                    request.COOKIES['jwt'] = str(new_tokens)
+                    request.COOKIES['refresh_jwt'] = str(refresh)
+                except TokenError as e:
+                    if isinstance(e, InvalidToken):
+                        return redirect('login')
+                    else:
+                        print(f"Error while refreshing tokens: {str(e)}")
+
             auth = CookieJWTAuthentication()
             try:
                 auth_result = auth.authenticate(request)
             except InvalidToken:
-                print("Token is invalid or expired. Redirecting to login page.")
                 return redirect('login')
 
             if auth_result is not None:
                 auth_user, _ = auth_result
                 if auth_user is not None:
                     request.user = auth_user
-
-
-class JWTRefreshTokenMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        print("JWTRefreshTokenMiddleware has been called")
-        if request.path != reverse('login'):
-            auth = CookieJWTAuthentication()
-            try:
-                auth_result = auth.authenticate(request)
-            except InvalidToken:
-                print("Access token is invalid or expired. Trying to refresh using refresh token.")
-                raw_refresh_token = request.COOKIES.get('refresh_jwt')
-                if raw_refresh_token is not None:
-                    try:
-                        print(f"Raw refresh token: {raw_refresh_token}")
-                        refresh = RefreshToken(raw_refresh_token)
-                        new_tokens = refresh.access_token
-                        response.set_cookie(key='jwt', value=str(new_tokens), httponly=True, secure=False)
-                        response.set_cookie(key='refresh_jwt', value=str(refresh), httponly=True, secure=False)
-                    except TokenError as e:
-                        print(f"Error while refreshing tokens: {str(e)}")
-                        print(f"Raw refresh token: {raw_refresh_token}")
-                        if isinstance(e, InvalidToken):
-                            print("Refresh token is invalid. Redirecting to login page.")
-                            return redirect('login')
-                        else:
-                            print(f"Error while refreshing tokens: {str(e)}")
-            else:
-                if auth_result is not None:
-                    auth_user, _ = auth_result
-                    if auth_user is not None:
-                        request.user = auth_user
-        return response
